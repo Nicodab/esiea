b7cbd4fa99fae969f964a1a1578ba0dcba24683c
Nicolas <nicolas.fischer78@gmail.com>
2023-10-18 18:03:34 +0200

    nouveaux elem réseau

---
+++ Linux/v0/file.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python3
+#! est un shebang et spécifie à l'interpréteur qlqch
+# Exos: afficher dir 1 à dir 50
+for i in range (100): #range(100) --> 0 à 99 et range (1,101) --> 1 à 101
+    print("Mon dir" + str(i), end='\0')
+
+# faut rendre le fichier éxecutable avec chmod +x file.py pour pouvoir le lancer
+#pour créer les 100 répertoire a partir de la sortie du fichier py on lance la commande ./file.py | xargs mkdir
+#--> Idem que pour supprimer on rajoute rmdir
+# xargs avec l'option -0 est un séparateur --> Evite de le faire avec --null
+
+# pour écrire dans le terminal et dans un fichier --> cmd /tee file
+
+
+
+#openssl f° a jour qdigest, openssl evb.h un truc comme ça qui est la version not deprecated
+# evpd ctx new ou evp getDigestbyName(passe le md5 en arg) et on lui passe l'arg ou alors evp-q-digest on peut l'utiliser sans initialisation
+
+
+
+#openssl f° a jour qdigest, openssl evb.h un truc comme ça qui est la version not deprecated
+# evpd ctx new ou evp getDigestbyName(passe le md5 en arg) et on lui passe l'arg ou alors evp-q-digest on peut l'utiliser sans initialisation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## utiliser ce script avec 
+#./file.py | xargs --null mkdir
+#./file.py | xargs --null mkdir
